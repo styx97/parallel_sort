3a1eb3635f6e66de5b00e1a4c53b905e2a902f3e
Rupak Sarkar <rupaksarkar.cs@gmail.com>
2018-03-08 13:40:37 +0530

    updated mergeclient for python3

--- MergeClient.py
+++ MergeClient.py
@@ -12,26 +12,27 @@ arraystring = ''
 print('Receiving data...' )
 while 1:
 
-	data = s.recv(4096)	#Receives data in chunks 
-	#print data 
-	arraystring += data	#Adds data to array string 
-	if ']' in data:	#When end of data is received
+        data = s.recv(4096)     #Receives data in chunks 
+        #print data 
+        d =  str(data) 
+        arraystring += d        #Adds data to array string 
+        if ']' in d:    #When end of data is received
 
-		break
+                break
 
 #eval turns the string into an array
-array = eval(arraystring)	
+array = eval(arraystring)       
 print('Data received, sorting array... ' )
 
 
 #Sorts the array which it is allocated 
-array = MergeSort.mergesort(array) 
+array = MergeSort.merge_sort(array) 
 print('Array sorted, sending data...') 
 
 
 #Converts array into string to be sent back to server 
 arraystring = repr(array) 
-s.sendall(arraystring)	#Sends array string 
-print 'Data sent.' 
+s.sendall(arraystring.encode('utf-8'))   #Sends array string 
+print('Data sent.')
 
 s.close()
