9db86bb5d464540a20571fcaae51781c528c3291
Rupak Sarkar <rupaksarkar.cs@gmail.com>
2018-03-19 00:46:46 +0530

    Update MergeClient.py

--- MergeClient.py
+++ MergeClient.py
@@ -1,69 +1,74 @@
-import socket 
-import MergeSort 
+import socket
+from MergeSort import merge_sort,merge
 import os, sys, time
 import math, random
 from multiprocessing import Process, Manager
-
-cores = 4 # for quad core cpu 
-
-
-# a wrapper function which appends the result of "merge_sort" to the "manager_list" 
+ 
+cores = 4 # for quad core cpu
+ 
+ 
+# a wrapper function which appends the result of "merge_sort" to the "manager_list"
 def merge_sort_multi( list_part ):
     manager_list.append( merge_sort( list_part ) )
-
-# a wrapper function which appends the result of "merge" to the "manager_list"                               
+ 
+# a wrapper function which appends the result of "merge" to the "manager_list"
 def merge_multi( list_part_left, list_part_right ):
     manager_list.append( merge(list_part_left, list_part_right ) )
-
-
-HOST = '192.168.1.1' 
-PORT = 50007 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect((HOST, PORT)) 
-
-
-#Receives arraystring in chunks 
-arraystring = '' 
+ 
+ 
+HOST = '192.168.1.1'
+PORT = 50007
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+ 
+ 
+#Receives arraystring in chunks
+arraystring = ''
 print('Receiving data...' )
 while 1:
-
-	data = s.recv(4096)	#Receives data in chunks 
-	#print data 
-	arraystring += data	#Adds data to array string 
-	if ']' in data:	#When end of data is received
-
-		break
-
+ 
+        data = s.recv(4096)     #Receives data in chunks
+        #print data
+        d = str(data)
+        arraystring += d        #Adds data to array string
+        if ']' in d:    #When end of data is received
+ 
+                break
+ 
 #eval turns the string into an array
-array = eval(arraystring)	
+array = eval(arraystring)
 print('Data received, sorting array... ' )
-
+#print("the received array is",array)
+ 
+ 
 if cores > 1:
+ 
+        manager = Manager()
+        manager_list  = manager.list()
         l = len(array)
-        ''' we collect the list element count and the time taken
-        for each of the procedures in a file '''
-        #f = open('mergesort-'+str(cores)+'.dat', 'a')
+ 
+        print("length of the array is ",l)
         print('Starting %d-core process'%cores)
         start_time = time.time()
-        
-        # divide the list in "cores" parts
+ 
+        # divide the list in number of parts equal to cores
         step = int( math.floor( l / cores ) )
         offset = 0
         p = []
-        
+ 
         for n in range(0, cores):
             ''' we create a new Process object and assign the "merge_sort_multi" function to it,
             using as input a sublist '''
             if n < cores - 1:
-                #pass part of a list to a process 
-                
+                #pass part of a list to a process
+ 
                 proc = Process( target=merge_sort_multi, args=( array[n*step:(n+1)*step], ) )
             else:
                 # get the remaining elements in the list
                 proc = Process( target=merge_sort_multi, args=( array[n*step:], ) )
-            
+ 
             p.append(proc)
-
+ 
         ''' http://docs.python.org/library/multiprocessing.html#multiprocessing.Process.start &
         http://docs.python.org/library/multiprocessing.html#multiprocessing.Process.join each Process '''
         for proc in p:
@@ -73,7 +78,7 @@ if cores > 1:
         print('Performing final merge...')
         start_time_final_merge = time.time()
         p = []
-        
+ 
         ''' For a core count greater than 2, we can use multiprocessing
         again to merge sublists in parallel '''
         if len(manager_list) > 2:
@@ -87,28 +92,28 @@ if cores > 1:
                 proc.start()
             for proc in p:
                 proc.join()
-        
-        # finally we have 2 sublists which need to be merged
+        # the last two sublists that needs to be merged
         array  = merge(manager_list[0], manager_list[1])
-        
-        # final_merge_time = time.time() - start_time_final_merge
-        # print 'Final merge duration : ', final_merge_time
-        # multi_core_time = time.time() - start_time
-        # # of course we double-check that we did everything right
-        
-        # print 'Sorted arrays equal : ', (a == single)
-        # print '%d-Core ended: %4.6f sec'%(cores, multi_core_time)
-          
-
-
-#Sorts the array which it is allocated 
-#array = MergeSort.mergesort(array) 
-#print('Array sorted, sending data...') 
-
-
-#Converts array into string to be sent back to server 
-arraystring = repr(array) 
-s.sendall(arraystring)	#Sends array string 
-print('Data sent.') 
-
-s.close()
+ 
+        final_merge_time = time.time() - start_time_final_merge
+        print('Final merge duration : ', final_merge_time)
+        multi_core_time = time.time() - start_time
+        print("multi core time",multi_core_time)
+        # print("the sorted array is ",array)
+        #print('Array sorted, sending data...')
+        #Converts array into string to be sent back to server
+        arraystring = repr(array)
+        s.sendall(arraystring.encode('utf-8'))  #Sends array string
+        print('Data sent.')
+        s.close()
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
